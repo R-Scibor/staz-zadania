12a5ef67197284d39f451d411c457bb0e4faaf56
Rafał Ścibor <zyrafka22@gmail.com>
2026-05-08 21:32:17 +0200

    feat: add ImGui/GLFW window and graph loading in main

--- Zadanie 2/src/main.cpp
+++ Zadanie 2/src/main.cpp
@@ -1,6 +1,76 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "Graph.hpp"
+#include "JSONParser.hpp"
+#include "GraphRenderer.hpp"
+#include "Config.hpp"
+
+#include <fstream>
+#include <sstream>
 
 int main() {
-    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "Blueprint Viewer", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    std::ifstream file("graph.json");
+    if (!file.is_open()) {
+        fprintf(stderr, "Could not open graph.json\n");
+        return 1;
+    }
+    std::stringstream ss;
+    ss << file.rdbuf();
+
+    JSONParser parser;
+    auto nodeMap = parser.parseJSON(ss.str());
+
+    Graph graph;
+    graph.assignLayers(nodeMap);
+    graph.calculateNodePositions();
+    auto nodes = graph.getSortedNodes();
+
+    GraphRenderer renderer(nodes);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        renderer.draw();
+
+        ImGui::Render();
+
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+        glClearColor(0.12f, 0.12f, 0.12f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
